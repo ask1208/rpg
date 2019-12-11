fc155859344bf42f7fa6f46747157dda87ab879a
ask1208 <t.ask.128@icloud.com>
2019-12-11 23:36:27 +0900

    訂正

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 "use strict";
 
-const FONT = "48px monospace";// 使用フォント 
+const	FONT	= "48px monospace";	//	使用フォント
 
-let gFrame = 0;               // 内部カウンタ
-let gImgMap;                  //画像、マップ
+let		gFrame = 0;					//	内部カウンタ
+let		gImgMap;					//	画像、マップ
 
-// タイマーイベント発生時の処理
+//	タイマーイベント発生処理
 function WmTimer()
 {
-  gFrame++;   
-                                 // 内部カウンタ加算
-  const ca = document.getElementById("main");// mainキャンバスの要素を取得
-  const g = ca.getContext("2d");// 2D描画コンテキストを取得
+	gFrame++;						//	内部カウンタを加算
 
-  for( let y=0;y < 16; y++ ){
-    for(let x =0; x < 16; x++ ){
-      g.drawImage( gImgMap, x * 32, y * 32 );//
-    }
-  }
-  g.font = FONT;                // 文字フォント設定
-  g.fillText("helllo world" + gFrame, gFrame/ 10, 64 );
-  
+	const	ca = document.getElementById( "main" );	//	mainキャンバス要素を取得
+	const	g = ca.getContext( "2d" );				//	22D描写テキストを取得
+
+	for( let y = 0; y < 16; y++ ){
+		for( let x = 0; x < 16; x++ ){
+			g.drawImage( gImgMap, x * 32, y * 32 );
+		}
+	}
+
+	g.font = FONT;									//	文字フォントを設定
+	g.fillText( "Hello World" + gFrame, gFrame / 10, 64 );
 }
-// ブラウザ起動イベント
+
+
+//	�u���E�U�N���C�x���g
 window.onload = function()
 {
-  gImgMap = new this.Image();gImgMap.src = "image/map.pmg"; //map画像読み込み
-  setInterval( function() { WmTimer() }, 33); // 33ms間隔で、WmTimerを呼び出す(約30.3fps))
+	gImgMap = new Image();	gImgMap.src = "img/map.png";		//	マップ画像を読み込み
+  setInterval( function(){ WmTimer() }, 33 );		//	33ms間隔でWmTimer()を呼び出す指示（30.3fps)
 }
+
 
